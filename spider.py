a2f59c4a46efa4ecb5714d3450eaa7926c42e941
Tianyu Shen <1796725346@qq.com>
2019-05-31 23:22:48 +0800

    改用csv保存文件

--- spider.py
+++ spider.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from lxml import etree
 import requests
 import time
-import json
+import csv
 
 def get_detail_page(page,url, xpath_of_a, xpath_of_next_page):
     browser = webdriver.Chrome()
@@ -15,7 +15,7 @@ def get_detail_page(page,url, xpath_of_a, xpath_of_next_page):
             href_list.append(href)
         next_page = browser.find_element_by_xpath(xpath_of_next_page)
         next_page.click()
-        time.sleep(1)
+        time.sleep(0.5)
     browser.close()
     return href_list
 
@@ -28,24 +28,31 @@ def get_detail_info(href_list):
         response = requests.get(href, headers=headers)
         html = etree.HTML(response.text)
         item['name'] = html.xpath('normalize-space(//*[contains(text(),"项目名称")]/text())').replace('项目名称：', '')
-        item['key_word']= html.xpath('normalize-space(//*[contains(text(),"关键词")]/text())').replace('项目关键词：', '')
         item['project_type'] = html.xpath('normalize-space(//*[contains(text(),"项目类型")]/text())').replace('项目类型：', '')
         item['school_name'] = html.xpath('normalize-space(//*[contains(text(),"所属学校")]/text())').replace('所属学校：', '')
         item['discipline_one'] = html.xpath('normalize-space(//*[contains(text(),"所属一级学科")]/text())').replace('所属一级学科：', '')
         item['discipline_two'] = html.xpath('normalize-space(//*[contains(text(),"所属二级学科")]/text())').replace('所属二级学科：', '')
         item['time'] = html.xpath('normalize-space(//*[contains(text(),"立项时间")]/text())').replace('立项时间：', '')
-        item['money'] = html.xpath('normalize-space(//*[contains(text(),"批准经费额度")]/text())').replace('批准经费额度：', '')
+        item['money'] = html.xpath('normalize-space(//*[contains(text(),"批准经费额度")]/text())').replace('批准经费额度：', '').replace('(元)', '')
         item['number_of_participants'] = int(html.xpath('count(//table[@class="table-itemstu"]//tr)-1'))
-        change_item = json.dumps(item, ensure_ascii=False)
-        with open('item.txt', 'a+') as file:
-                file.write(change_item)
-
+        with open('data1.csv', 'a') as csvfile:
+            fieldnames = ['name', 'project_type', 'school_name', 'discipline_one', 'discipline_two',
+                          'time', 'money', 'number_of_participants']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow({'name': item['name'],
+                             'project_type': item['project_type'],
+                             'school_name': item['school_name'], 'discipline_one': item['discipline_one'],
+                             'discipline_two': item['discipline_one'],
+                             'time': item['time'],
+                             'money': item['money'],
+                             'number_of_participants': item['number_of_participants']})
+        print(item)
+                    
 if __name__ == '__main__':
-
     url = 'http://gjcxcy.bjtu.edu.cn/LXItemList.aspx'
     xpath_of_a = '//div[@class="List_Nrz FL"]/a'
     xpath_of_next_page = '//*[contains(text(),"下一页")]'
-    page = 1577
+    page = 1951
 
     href_list = get_detail_page(page, url, xpath_of_a, xpath_of_next_page)
     get_detail_info(href_list)
